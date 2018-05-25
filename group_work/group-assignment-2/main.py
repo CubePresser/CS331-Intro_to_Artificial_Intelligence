2057914e502bda5aca1c27236ec3bb38ee3f9cb6
Brett Case <brett.case97@gmail.com>
2018-05-25 12:56:40 -0700

    reads from file and strips punctuations

--- group_work/group-assignment-2/main.py
+++ group_work/group-assignment-2/main.py
@@ -1,14 +1,22 @@
-import os
+import os, string
 from sys import argv
 
 def readFromFile(fileName):
-    
+    file = open(fileName, "r") #open our given file with read only permissions
+    for line in file:
+        return #delete this its just for now
+        #TODO: separate line into words and num
+        #alphebetize words in master list
+        
     return
 
 def stripPunctuation(sentence):
-    
-    return
+    sentence = sentence.translate(None, string.punctuation)
+    return sentence
 
 if __name__ == "__main__": #treat this as a main function and keep at bottom of file
     fileName = argv[1] #first arguement after script name should be what file is wanted
-    print("butts")
+    readFromFile(fileName)
+
+    words = "This isn't a punctuation test. WOW! ..,, Coor's"
+    print(stripPunctuation(words).lower())
