26baa7e0e5e00ef0e6074bb78b7bac4da192c8a0
jonesjonathan <jonesjon@oregonstate.edu>
2018-05-25 12:38:12 -0700

    Started assignment. Switching to Brett's machine.

---
+++ group_work/group-assignment-2/main.py
@@ -0,0 +1,14 @@
+import os
+from sys import argv
+
+def readFromFile(fileName):
+    
+    return
+
+def stripPunctuation(sentence):
+    
+    return
+
+if __name__ == "__main__": #treat this as a main function and keep at bottom of file
+    fileName = argv[1] #first arguement after script name should be what file is wanted
+    print("butts")
